c9204b97dfd4d377c4c5562fe39aed2cd26987cb
Екатерина <Katderl@ukr.net>
2023-01-16 12:38:52 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NotFound from '../../pages/NotFound';
-import SharedLayout from '../SharedLayout/SharedLayout';
+
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import NotFound from 'pages/NotFound';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
 
-const Home = lazy(() => import('../../pages/Home/Home'));
-const NewContact = lazy(() => import('../../pages/NewContact/NewContact'));
-const EditContact = lazy(() => import('../../pages/EditContact/EditContact'));
+const Home = lazy(() => import('pages/Home/Home'));
+const NewContact = lazy(() => import('pages/NewContact/NewContact'));
+const EditContact = lazy(() => import('pages/EditContact/EditContact'));
 
 export default function App() {
   return (
